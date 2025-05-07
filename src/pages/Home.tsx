c821251ce8ef903f412b787df275910c984f19af
bart <bart_klumperman@Live.nl>
2025-05-07 20:29:42 +0200

    poc for fetching from inaturalist need to make it use reactQuery and put code in correct places next

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home: React.FunctionComponent = () => {
-  return <div className="text-4xl">Home</div>;
+  const [reptiles, setReptiles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState("1");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const baseUrl = "https://api.inaturalist.org/v1";
+
+    const params = new URLSearchParams({
+      taxon_id: "26036",
+      per_page: "30",
+      page,
+      order: "desc",
+      order_by: "created_at",
+      photos: "true",
+    });
+
+    const url = `${baseUrl}/observations?${params.toString()}`;
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setReptiles(data.results);
+        console.log(reptiles);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(
+          error instanceof Error ? error.message : "something went wrong"
+        );
+        console.error("Error fetching reptile data:", error);
+      });
+  }, [page]);
+
+  return (
+    <div className="text-4xl">
+      {isLoading && <div>Loading...</div>}
+      {error && <div className="text-red">{error}</div>}
+      {!isLoading && !error && (
+        <div>
+          {reptiles.map((reptile) => (
+            <div>
+              <img src={reptile.photos[0].url ?? ""} className="w-50 h-50" />
+              <h2 key={reptile.id}>{reptile.species_guess}</h2>
+            </div>
+          ))}
+        </div>
+      )}
+      <button
+        className="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setPage(Math.floor(Math.random() * 10 + 1).toString())}
+      >
+        Next page
+      </button>
+    </div>
+  );
 };
 
 export default Home;
